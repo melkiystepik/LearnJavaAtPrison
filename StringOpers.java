6558dcee68ad4c97f09cd14f7844f8d7317676b2
melkiystepik <stepigandruxer@gmail.com>
2023-07-13 11:27:59 +0300

    Create StringOpers.java

---
+++ StringOpers.java
@@ -0,0 +1,15 @@
+package javaLearnAtPrison;
+public class StringOpers {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int x=0, y=1, z=3;
+		String s = "x, y, z";
+		System.out.println("s+x+y+z"+s+x+y+z);
+		System.out.println(x+" "+s);
+		s+="(summed)=";
+		System.out.println(s+(x+y+z));
+		System.out.println(" "+x);
+		}
+
+}
